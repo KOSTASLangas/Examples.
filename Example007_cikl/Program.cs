ca3d64bdcd7469da8f3e79d314269f7f5832f535
Kostas <langas.kostya@mail.ru>
2022-07-10 20:46:38 +0500

    Создание цикла, рисование треугольника

---
+++ Example007_cikl/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear ();
+
+int xo=1, yo=1, //Координаты первой точки
+    xa=1, ya=30, //Координаты второй точки
+    xb=60, yb=30; //Координаты второй точки
+
+Console.SetCursorPosition (xo,yo);
+Console.WriteLine ("+");
+
+Console.SetCursorPosition (xa,ya);
+Console.WriteLine ("+");
+
+Console.SetCursorPosition (xb,yb);
+Console.WriteLine ("+");
+
+
